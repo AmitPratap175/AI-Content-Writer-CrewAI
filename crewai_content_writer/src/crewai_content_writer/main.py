536566dba345ffccc9cdd16f1daeac624cff3d1f
Amit Pratap <00amitpratap@gmail.com>
2025-02-21 11:05:20 +0530

    Deployment grade code added

--- crewai_content_writer/src/crewai_content_writer/main.py
+++ crewai_content_writer/src/crewai_content_writer/main.py
@@ -5,27 +5,39 @@ import warnings
 from datetime import datetime
 
 from crewai_content_writer.crew import CrewaiContentWriter
+import gradio as gr
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+## Simple way to initialize history for the ChatInterface
+chatbot = gr.Chatbot(height=500, scale=10,value = [[None, "Hi, what article do you want me to write?"]])
 
 # This main file is intended to be a way for you to run your
 # crew locally, so refrain from adding unnecessary logic into this file.
 # Replace with inputs you want to test with, it will automatically
 # interpolate any tasks and agents information
 
+def chat(message, history, return_buffer=True):
+    inputs = {
+        'topic': message,
+        # 'current_year': str(datetime.now().year)
+    }
+    buffer = ''
+    
+    try:
+        buffer = str(CrewaiContentWriter().crew().kickoff(inputs=inputs))
+    except Exception as e:
+        return f"An error occurred while running the crew: {e}"
+    
+    return buffer
+
+
 def run():
     """
     Run the crew.
     """
-    inputs = {
-        'topic': 'AI LLMs',
-        'current_year': str(datetime.now().year)
-    }
-    
-    try:
-        CrewaiContentWriter().crew().kickoff(inputs=inputs)
-    except Exception as e:
-        raise Exception(f"An error occurred while running the crew: {e}")
+    gr.ChatInterface(chat, chatbot=chatbot).queue().launch(debug=True, share=True, inline=False)
+
 
 
 def train():
